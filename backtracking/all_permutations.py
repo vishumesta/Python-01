d6e906d38c18a960a8105e2d5ec38a7453244008
vishwajeet mesta <40387440+vishumesta@users.noreply.github.com>
2026-01-08 17:07:14 +0530

    Update all_permutations.py

--- backtracking/all_permutations.py
+++ backtracking/all_permutations.py
@@ -8,7 +8,6 @@ where n denotes the length of the given sequence.
 
 from __future__ import annotations
 
-Hi hello
 
 def generate_all_permutations(sequence: list[int | str]) -> None:
     create_state_space_tree(sequence, [], 0, [0 for i in range(len(sequence))])
