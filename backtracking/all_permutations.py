6e3fa83388f01ebb2df2103886dedef70f5c831f
vishwajeet mesta <40387440+vishumesta@users.noreply.github.com>
2026-01-08 17:00:43 +0530

    Update all_permutations.py

--- backtracking/all_permutations.py
+++ backtracking/all_permutations.py
@@ -12,7 +12,6 @@ from __future__ import annotations
 def generate_all_permutations(sequence: list[int | str]) -> None:
     create_state_space_tree(sequence, [], 0, [0 for i in range(len(sequence))])
 
-Hi Heloo
 
 def create_state_space_tree(
     sequence: list[int | str],
